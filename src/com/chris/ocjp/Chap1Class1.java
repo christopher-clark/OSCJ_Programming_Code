2f35679443f33d97aae6d6ebe6663a6c13a746fd
Christopher Clark <g.kanizaj.clark@gmail.com>
2016-02-01 20:44:06 +1100

    Added third println

--- src/com/chris/ocjp/Chap1Class1.java
+++ src/com/chris/ocjp/Chap1Class1.java
@@ -4,5 +4,6 @@ public class Chap1Class1 {
 	public void method1(){
 		System.out.println("Chapter 1 ");
 		System.out.println("Chapter 1 line 2 ");
+		System.out.println("Chapter 1 line 3 ");
 	}
 }
