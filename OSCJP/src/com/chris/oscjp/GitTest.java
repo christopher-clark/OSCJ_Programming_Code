5c90c9e1259f7d4487046fc501df9f0e162fb2ac
christopher-clark <g.kanizaj.clark@gmail.com>
2016-02-02 18:14:41 +1100

    readding GitTest

---
+++ OSCJP/src/com/chris/oscjp/GitTest.java
@@ -0,0 +1,11 @@
+package com.chris.oscjp;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I Love Git");
+		System.out.println("I Love Git twice as much");
+	}
+
+}
