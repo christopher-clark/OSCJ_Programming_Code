6d1c3b663826b2a00a511d775bf99da70df1d11d
christopher-clark <g.kanizaj.clark@gmail.com>
2016-02-08 21:18:45 +1100

    Committing Animal Class - Chapter 2

---
+++ OSCJP/src/com/chris/oscjp/chapter2/Animal.java
@@ -0,0 +1,11 @@
+package com.chris.oscjp.chapter2;
+
+public class Animal {
+	
+	public void showName(){
+		System.out.println("My name is Animal");
+	}
+	public void eat() {
+		System.out.println("Generic Animal Eating Generically");
+	}
+}
