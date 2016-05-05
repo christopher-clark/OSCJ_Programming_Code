2d7bb3ddfc5725030e62d59e6a2a44631da489de
christopher-clark <g.kanizaj.clark@gmail.com>
2016-05-05 21:27:01 +1000

    Committing Foo, Frog, Animal & TestAnimal to Master

--- OSCJP/src/com/chris/oscjp/chapter1/TestAnimal.java
+++ OSCJP/src/com/chris/oscjp/chapter1/TestAnimal.java
@@ -10,14 +10,15 @@ public class TestAnimal {
 		makeNoises(c);
 		makeNoises(d);
 		Animal felix = c;
-	
+		System.out.println(c.name);
 		c.makeNoise();
 		if(c instanceof Animal){
 			System.out.println("cat is an Animal");
 		}
 	}
 	public static void makeNoises(Animal a){
-		a.makeNoise();
+		//a.makeNoise();
+		System.out.println(a.name);
 	}
 
 }
